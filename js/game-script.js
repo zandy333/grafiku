1f5df6cf900da10b9fe07d9fd02cacec753dfa62
zandy333 <67714054+zandy333@users.noreply.github.com>
2025-12-07 11:42:28 +0530

    Add files via upload

--- js/game-script.js
+++ js/game-script.js
@@ -207,17 +207,17 @@ window.onload=function() {
   function displayLandscape() { 
 
     for (let i = 0; i < tbl_1.length; i++) {
-      tbl_1[i].style.border = "1.0vh solid #ffffff";
+      tbl_1[i].style.border = "1.0vh solid #2a3da7";
       //tbl_1[i].style.width = "86vh";
       //tbl_1[i].style.height = "86vh";
     };
 
     for (let i = 0; i < td_1.length; i++) {
-      td_1[i].style.border = "0.5vh solid #ffffff";
+      td_1[i].style.border = "0.5vh solid ##2a3da7";
     };
 
     for (let i = 0; i < td_2.length; i++) {
-      td_2[i].style.border = "0.5vh solid #ffffff";
+      td_2[i].style.border = "0.5vh solid ##2a3da7";
       td_2[i].style.width = "20vh";
       td_2[i].style.height = "20vh";
     };
